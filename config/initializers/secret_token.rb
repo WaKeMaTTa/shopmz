acbb0a82ca77c8b37489856c0a929d19e8d65b7e
Mohamed Ziata (WaKeMaTTa) <m.ziata@hotmail.com>
2014-02-25 17:51:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopmz::Application.config.secret_key_base = '9f95c3981a710344d46d4bfd8e8f47b7584b43e9d1b5614034161c5eefc5aa514b7437da6868cf1b3042bcb54fc2bbf8d809850e38b5aaca3fd5cb08211c01fd'
