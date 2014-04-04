2ab852792d26b3754c676cd2a590a6503b659c80
WaKeMaTTa <m.ziata@hotmail.com>
2014-04-04 12:27:22 +0200

    Change suggested by the teacher.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 	def create
-		user = User.find_by_email(params[:login][:email])
+		user = User.find_by(email: params[:login][:email])
 		if user && user.authenticate(params[:login][:password])
 			session[:user_id] = user.id
 			redirect_to root_url, :notice => "Logged in!"
