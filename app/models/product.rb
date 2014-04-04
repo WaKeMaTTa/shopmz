eb228c4ef9601eee24a709a8132c0d274b7ba887
WaKeMaTTa <m.ziata@hotmail.com>
2014-04-04 14:34:06 +0200

    Validations (part 1)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ActiveRecord::Base
 	has_many :line_items
 	has_many :orders, through: :line_items
+
+	#include ActiveModel::Validations
+	#validates_with ProductCategoryValidator
+	validates_presence_of :name
+	validates_uniqueness_of :name
+	validates_presence_of :quantity
+	validates_numericality_of :quantity
+	validates_presence_of :registred_at
+	validates_presence_of :price
+	validates_numericality_of :price
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	validates_presence_of :first_name
+	validates_presence_of :last_name
+	validates_presence_of :email
+	validates_numericality_of :email
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+	validates_uniqueness_of :email
 	validates_presence_of :password, :on => :create
+	validates_length_of :password, :minimum => 8
+	validates_format_of :password, :with => /[a-z0-9]{2,}/i
 end
