943e01019779be9d670e439c6cfcac3d3459b214
WaKeMaTTa <m.ziata@hotmail.com>
2014-04-09 18:27:34 +0200

    Al mismo tiempo, se deberá descontar la cantidad del producto que ha sido comprado

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,10 +6,15 @@ class LineItem < ActiveRecord::Base
 	validates :quantity, numericality: { only_integer: true }
 
 	before_save :set_price_to_products
+	before_save :reduce_stock
 
 	private
 
 		def set_price_to_products
 			self.price = self.product.price
+		end
+
+		def reduce_stock
+			Product.update(self.product.id, quantity: (self.product.quantity - self.quantity))
 		end
 end
