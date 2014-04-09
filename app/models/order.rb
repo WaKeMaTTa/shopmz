b0e59586ef62e34b097e08cb7d25c9e128f4ef11
WaKeMaTTa <m.ziata@hotmail.com>
2014-04-09 18:07:28 +0200

    Fix Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
 	has_many :line_items
 	has_many :products, through: :line_items
-	accepts_nested_attributes_for :line_items, :allow_destroy => true, :reject_if => lambda { |li| li[:product_id].blank? or li[:quantity].blank? or li[:price].blank? }
+	accepts_nested_attributes_for :line_items, :allow_destroy => true, :reject_if => lambda { |li| li[:product_id].blank? or li[:quantity].blank? }
 
 	validates :customer_name, :delivery_city, :delivery_address, :payment_method, :status, presence: true
 	validates :status, inclusion: { in: %w(pending sent received), message: "'%{value}' is not valid status." }
