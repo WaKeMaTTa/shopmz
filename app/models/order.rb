958663d46d2ca200afdf041038c94567b7ad0b5d
WaKeMaTTa <m.ziata@hotmail.com>
2014-04-09 18:40:19 +0200

    Auto-set status in New Order. Status only change in edit page

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,15 @@ class Order < ActiveRecord::Base
 	has_many :products, through: :line_items
 	accepts_nested_attributes_for :line_items, :allow_destroy => true, :reject_if => lambda { |li| li[:product_id].blank? or li[:quantity].blank? }
 
-	validates :customer_name, :delivery_city, :delivery_address, :payment_method, :status, presence: true
-	validates :status, inclusion: { in: %w(pending sent received), message: "'%{value}' is not valid status." }
+	validates :customer_name, :delivery_city, :delivery_address, :payment_method, presence: true
+	#validates :status, inclusion: { in: %w(pending sent received), message: "'%{value}' is not valid status." }
 	validates :payment_method, inclusion: { in: %w(cash card paypal), message: "'%{value}' is not valid payment method." }
+
+	before_save :set_order_status_pending, on: :create
+
+	private
+
+		def set_order_status_pending
+			self.status = "pending"
+		end
 end
