4c9ff8d30b5a9a0884cc7c81a8fe81f4a4717f94
WaKeMaTTa <m.ziata@hotmail.com>
2014-04-15 17:07:29 +0200

    Define 'ProductsHelper::get_manufacturers'

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+	def get_manufacturers(with_blank_option = false)
+		if with_blank_option == false
+			manufacturers = []
+		else
+			manufacturers = ['']
+		end
+
+		manufacturers += Product.select("manufacturer").map {|i| i.manufacturer }
+
+		return manufacturers
+	end
 end
